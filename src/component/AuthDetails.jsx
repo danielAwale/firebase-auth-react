a4b1f6bf0a7ece00f55d8d38cc9f82c943bd7006
Daniel Awale <danielmadhukarawale@gmail.com>
2023-03-18 10:53:30 -0700

    contain a auth details route

---
+++ src/component/AuthDetails.jsx
@@ -0,0 +1,28 @@
+import { onAuthStateChanged } from "firebase/auth";
+import React, { useState, useEffect } from "react";
+import { auth } from "../firebase";
+
+const AuthDetails = () => {
+  const [authUser, setAuthUser] = useState(null);
+
+  useEffect(() => {
+    const listen = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setAuthUser(user);
+      } else {
+        setAuthUser(null);
+      }
+    });
+  });
+  return (
+    <div>
+      {authUser ? (
+        <>We have a secret word for you! It is FIREBASE</>
+      ) : (
+        <>You do not have access to the secret word!!!</>
+      )}
+    </div>
+  );
+};
+
+export default AuthDetails;
