2c681b4dadfa4779bc1bf7eeeb5a6d03ff0eb578
Daniel Awale <danielmadhukarawale@gmail.com>
2023-03-17 19:19:54 -0700

    change some code and will be able to signup one use

--- src/component/SignUp.jsx
+++ src/component/SignUp.jsx
@@ -1,79 +1,78 @@
-import { useState } from "react";
+import React, { useState } from "react";
+import { createUserWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../firebase";
 
-function SignUp() {
-  const [name, setName] = useState("");
+const SignIn = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  function handleSubmit(event) {
-    event.preventDefault();
-    // Handle form submission here with Firebase Authentication
-  }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Add code to handle sign-in here
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        console.log(userCredential);
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+  };
 
   return (
-    <div className="rounded-lg shadow-lg p-6">
-      <h2 className="text-lg font-medium mb-4">Sign up</h2>
-      <form>
-        <div className="mb-4">
-          <label className="block text-white font-medium mb-2" htmlFor="name">
-            Name
-          </label>
-          <input
-            className="border rounded-lg py-2 px-3 w-full"
-            type="text"
-            name="name"
-            id="name"
-            required
-          />
-        </div>
-        <div className="mb-4">
-          <label className="block text-white font-medium mb-2" htmlFor="email">
+    <form onSubmit={handleSubmit} className="w-full max-w-sm mx-auto py-12">
+      <div className="md:flex md:items-center mb-6">
+        <div className="md:w-1/3">
+          <label
+            htmlFor="email"
+            className="block text-white font-bold md:text-right mb-1 md:mb-0 pr-4"
+          >
             Email
           </label>
+        </div>
+        <div className="md:w-2/3">
           <input
-            className="border rounded-lg py-2 px-3 w-full"
             type="email"
-            name="email"
             id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
             required
           />
         </div>
-        <div className="mb-4">
+      </div>
+      <div className="md:flex md:items-center mb-6">
+        <div className="md:w-1/3">
           <label
-            className="block text-white font-medium mb-2"
             htmlFor="password"
+            className="block text-white font-bold md:text-right mb-1 md:mb-0 pr-4"
           >
             Password
           </label>
+        </div>
+        <div className="md:w-2/3">
           <input
-            className="border rounded-lg py-2 px-3 w-full"
             type="password"
-            name="password"
             id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
             required
           />
         </div>
-        <div className="flex items-center">
-          <input
-            className="mr-2"
-            type="checkbox"
-            name="agree"
-            id="agree"
-            required
-          />
-          <label className="text-white font-medium" htmlFor="agree">
-            I agree to the terms and conditions
-          </label>
-        </div>
-        <div className="mt-6">
-          <button className="bg-blue-500 hover:bg-blue-600 text-white font-medium rounded-lg py-2 px-4">
-            Sign up
+      </div>
+      <div className="md:flex md:items-center">
+        <div className="md:w-1/3"></div>
+        <div className="md:w-2/3">
+          <button
+            type="submit"
+            className="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
+          >
+            SignUp
           </button>
         </div>
-      </form>
-    </div>
+      </div>
+    </form>
   );
-}
+};
 
-export default SignUp;
+export default SignIn;
